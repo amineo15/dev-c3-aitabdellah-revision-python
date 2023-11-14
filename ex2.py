1fe0233dac787bbb4d48ff9ce9e771fa3e8ffaa9
Amine <mohamedamine.ma919@gmail.com>
2023-11-14 15:58:18 +0100

    ex2

---
+++ ex2.py
@@ -0,0 +1,7 @@
+phrase = input("Veuillez saisir une phrase : ")
+phrase_maj = phrase.upper()
+print("En majuscules : ", phrase_maj)
+phrase_min = phrase.lower()
+print("En minuscules : ", phrase_min)
+nombre_mots = len(phrase)
+print("Nombre de mots : ", nombre_mots)
